f4833a6ff030d4d37b596b7701cd787974f2b5c9
Maxim Denisov <denisovmax1988@gmail.com>
2018-03-03 14:50:29 +0300

    HMR

--- shared/apps/about/index/index.js
+++ shared/apps/about/index/index.js
@@ -6,8 +6,18 @@ const About = () => (
   <div className={styles.card}>
     <h1>About</h1>
     <div className={styles.description}>
-      We also need to edit our server.jsx file and call Helmet.renderStatic() after
-      ReactDOMServer.renderToString to be able to use the head data in our prerender.
+      <p className={styles.line1}>
+        We also need to edit our server.jsx file and call Helmet.renderStatic() after
+        ReactDOMServer.renderToString to be able to use the head data in our prerender.
+      </p>
+      <p>
+        We also need to edit our server.jsx file and call Helmet.renderStatic() after
+        ReactDOMServer.renderToString to be able to use the head data in our prerender.
+      </p>
+      <p>
+        We also need to edit our server.jsx file and call Helmet.renderStatic() after
+        ReactDOMServer.renderToString to be able to use the head data in our prerender.
+      </p>
     </div>
   </div>
 );
