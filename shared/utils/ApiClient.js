8a6b690c1fa3ad66589dd6209834f70c64226276
Maxim Denisov <denisovmax1988@gmail.com>
2018-03-18 12:08:10 +0300

    Refactoring Api 😎

--- shared/utils/ApiClient.js
+++ shared/utils/ApiClient.js
@@ -1,10 +1,33 @@
 import fetch from 'isomorphic-fetch';
 
-import { checkStatus, parseJSON } from './network';
-
 export const API_URL = (typeof window === 'undefined' || process.env.NODE_ENV === 'test')
   ? 'http://localhost:8000/api'
   : '/api';
+
+export function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  return response
+    .json()
+    .then(json => Promise.reject({ // eslint-disable-line
+      status: response.status,
+      ok: false,
+      statusText: response.statusText,
+      message: json.message,
+    }));
+}
+
+export function parseJSON(response) {
+  if (response.status === 204 || response.status === 205) {
+    return null;
+  }
+
+  // return response.json().then(json => ({ json, response }));
+  return response.json().then(json => json);
+}
+
 
 function ApiClient(endpoint, method = 'post', body) {
   return fetch(`${API_URL}${endpoint}`, {
